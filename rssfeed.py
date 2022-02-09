f958f3cb5149bc85a5920a7eb76636ac20350bfb
David Blevins <cda.cottage@gmail.com>
2022-02-09 14:35:50 -0600

    walk the  OPLM tree

--- rssfeed.py
+++ rssfeed.py
@@ -1,17 +1,18 @@
 import feedparser
 import opml
 
-url = "http://news.ycombinator.com/rss"
-
 outline = opml.parse("feedly-7afe47d6-236f-442b-a87c-1e8e7c31ac59-2022-02-09.opml")
 
-print(outline[0].title)
-print(outline[0][0].title)
+for category in outline:
+    print(category.title)
 
-url = outline[0][0].xmlUrl
-d = feedparser.parse(url)
+    for site in category:
+        print(" " + site.title)
 
-for entry in d.entries:
-    print(entry.title)
+        feedUrl = site.xmlUrl
+        rssFeed = feedparser.parse(feedUrl)
+
+        for entry in rssFeed.entries:
+            print("  " + entry.title)
 
 
