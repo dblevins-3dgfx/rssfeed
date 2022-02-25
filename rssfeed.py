4faaf8b7a66ad6135164ff87f26034343b014d7d
David Blevins <cda.cottage@gmail.com>
2022-02-25 10:58:19 -0600

    Generalize outline traversal

--- rssfeed.py
+++ rssfeed.py
@@ -1,18 +1,17 @@
 import feedparser
 import opml
 
-outline = opml.parse("feedly-7afe47d6-236f-442b-a87c-1e8e7c31ac59-2022-02-09.opml")
+def traverse(node, visitor, parent):
+    visitor(node, parent)
+    for child in node:
+        traverse(child, visitor, node)
+            
+root = opml.parse("feedly-7afe47d6-236f-442b-a87c-1e8e7c31ac59-2022-02-09.opml")
 
-for category in outline:
-    print(category.title)
+def visit_node(o, p):
+    indent = ""
+    if (hasattr(o, 'type') and o.type == 'rss'):
+        indent = "  "
+    print(indent + o.title)
 
-    for site in category:
-        print(" " + site.title)
-
-        feedUrl = site.xmlUrl
-        rssFeed = feedparser.parse(feedUrl)
-
-        for entry in rssFeed.entries:
-            print("  " + entry.title)
-
-
+traverse(root, visit_node, None)
